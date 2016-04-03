86c649f23b29161bd2d68e66c5ff41efa36774c1
Lukas Ruge <lukeslog@googlemail.com>
2016-04-03 15:45:15 +0200

    it can twitter.

--- iceboxtwitter.js
+++ iceboxtwitter.js
@@ -1,1 +1,25 @@
-//HERE BE CODE
+var Twitter = require('twitter');
+var constants = require('./constants.js');
+
+var client = new Twitter({
+  consumer_key: constants.getConsumerKey(),
+  consumer_secret: constants.getConsumerSecret(),
+  access_token_key: constants.getTokenKey(),
+  access_token_secret: constants.getTokenSecret()
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+client.post('statuses/update', {status: 'Test. Test.'},  function(error, tweet, response){
+  if(error) {
+    console.log(JSON.stringify(error));
+    throw error;
+  }
+  console.log(tweet);  // Tweet body.
+  console.log(response);  // Raw response object.
+});

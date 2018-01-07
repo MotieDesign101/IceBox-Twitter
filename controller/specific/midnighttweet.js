61b3884d8cc7ccc23f17b2a3ed4937eba65a108f
Lukas Ruge <lukeslog@googlemail.com>
2018-01-07 19:08:27 +0100

    moar midnight madness

--- controller/specific/midnighttweet.js
+++ controller/specific/midnighttweet.js
@@ -1,4 +1,33 @@
 var helper = require('./../controllerhelper.js');
+var request = require("request");
+
+var probability = 10;
+
+var caffeinTweets = [
+   'Das sind summa-sumarum <mg> mg Koffein.',
+   'Insgesammt wurden <mg> mg Koffein Konsumiert.',
+   '<mg> mg Koffein ist dabei in Leute gekommen.',
+   '<mg> mg Koffein ist dabei unter die Leute gekommen. 🍹.'
+];
+
+var sugarTweets = [
+    'Das entspricht <mg> g Zucker.',
+    'Zucker... Zucker ist gut. Heute waren es <mg> g Zucker.',
+    'Zuckerkonsum: Heute entspricht dieser <wuerfel> Würfelzucker. Wie viele Saarland sind das?',
+    'Heute wurden <mg> g Zucker konsumiert. Das sind <mg2> g Ost-Zucker <mg3> g Ost-Zucker auf dem Schwarzmarkt.'
+];
+
+var kallorienTweet = [
+    'Zählt jemand Kallorien? Das waren <kcal> kcal heute.',
+    'Oder auch <kcal> kcal.',
+    'Das sind insgesammt <kcal> kcal. Guten Appetit.',
+    'Heute sind daher <kcal> kcal Energie in Leute übergegangen.'
+];
+
+var popularDrink = [
+    'Der beliebteste Drink war heute <name>.',
+    'Besonders beliebt war <name>.',
+]
 
 exports.potentiallyAddTweet = function(tweetsToSendOut, consumptionData) {
   if(helper.isMidnight()) {
@@ -14,6 +43,7 @@ function addTweet(tweetsToSendOut, consumptionData) {
   getAmountOfDrinks(tweetsToSendOut, consumptionData);
   getMostConsumedDrink(tweetsToSendOut, consumptionData);
   getAmountOfCaffeein(tweetsToSendOut, consumptionData);
+  getAmountOfSugar(tweetsToSendOut, consumptionData);
 }
 
 function getAmountOfDrinks(tweetsToSendOut, consumptionData) {
@@ -26,9 +56,109 @@ function getAmountOfDrinks(tweetsToSendOut, consumptionData) {
 }
 
 function getMostConsumedDrink(tweetsToSendOut, consumptionData) {
+  var howManyDrinks = consumptionData.length;
+  var allDrinks = [];
+  if(howManyDrinks > 15 && helper.randomTrueFalse (probability)) {
+    for( var i in consumptionData) {
+      allDrinks.push(consumptionData[i]['name'])
+    }
+    console.log(allDrinks);
+  }
+  var name = getMostCommonFromArray(allDrinks);
+  console.log(name);
+  var popDrinkTweet = helper.getRandomTweet(popularDrink);
+  popDrinkTweet = popDrinkTweet.replace('<name>', name);
+  tweetsToSendOut.push(popDrinkTweet);
+}
 
+function getMostCommonFromArray(array)
+{
+    if(array.length == 0){
+      return null;
+    }
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+      var el = array[i];
+      if(modeMap[el] == null) {
+        modeMap[el] = 1;
+      } else {
+        modeMap[el]++;
+      }
+      if(modeMap[el] > maxCount)
+      {
+        maxEl = el;
+        maxCount = modeMap[el];
+      }
+    }
+    return maxEl;
 }
 
 function getAmountOfCaffeein(tweetsToSendOut, consumptionData) {
+  console.log("bla");
+  if(helper.randomTrueFalse (probability)) {
+    var cafSum = 0.0;
+    getIngredientData( function(ingredientData) {
+      for( var i in consumptionData) {
+        var consumption = consumptionData[i];
+        var barcode = consumption['barcode'];
+        var specificIngredient = ingredientData[barcode];
+        var specificCaffein = specificIngredient['caffeine'];
+        cafSum = cafSum + specificCaffein;
+      }
+      var caffeinTweet = helper.getRandomTweet(caffeinTweets);
+      caffeinTweet = caffeinTweet.replace('<mg>', cafSum.toFixed(2));
+      console.log(caffeinTweet);
+      tweetsToSendOut.push(caffeinTweet);
+    });
+  }
+}
 
+function getAmountOfSugar(tweetsToSendOut, consumptionData) {
+  console.log("bla Sugar");
+  if(true) {
+  //if(helper.randomTrueFalse (probability)) {
+    var cafSum = 0.0;
+    getIngredientData( function(ingredientData) {
+      for( var i in consumptionData) {
+        var consumption = consumptionData[i];
+        var barcode = consumption['barcode'];
+        var specificIngredient = ingredientData[barcode];
+        var specificCaffein = specificIngredient['sugar'];
+        cafSum = cafSum + specificCaffein;
+      }
+      var sugarTweet = helper.getRandomTweet(sugarTweets);
+      sugarTweet = sugarTweet.replace('<mg>', cafSum.toFixed(2));
+      sugarTweet = sugarTweet.replace('<mg2>', (cafSum*2).toFixed(2));
+      sugarTweet = sugarTweet.replace('<mg3>', (cafSum*4).toFixed(2));
+      sugarTweet = sugarTweet.replace('<wuerfel>', (cafSum/3).toFixed(2));
+      console.log(sugarTweet);
+      tweetsToSendOut.push(sugarTweet);
+    });
+  }
+}
+
+function getIngredientData(callback) {
+  console.log("get ingredient data");
+  var url = "http://icebox.nobreakspace.org:8087/ingred";
+  webrequest(url, function(body) {
+    callback(body);
+  });
+}
+
+function webrequest(requestUrl, callback) {
+  console.log("web request");
+  request({
+    url: requestUrl,
+    json: true
+  }, function(error, response, body) {
+    if (!error && (response.statusCode === 200 || response.statusCode === 201)) {
+      console.log(response.statusCode);
+      callback(body);
+    } else {
+        console.log("adding iceboxdown...");
+        callback();
+      }
+  });
 }
